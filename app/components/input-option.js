c79b834e67130865abcc819140014e8780e2c9d2
Joshua Opata <opatajoshua@gmail.com>
2021-11-19 16:11:47 +0000

    code formatting

--- app/components/input-option.js
+++ app/components/input-option.js
@@ -2,13 +2,16 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 
 export default class InputOptionComponent extends Component {
-    /** getter to to check whether to use input or text area */
-    get isInput(){
-        return this.args.question.multiline===false||this.args.question.multiline==='false'
-    }
+  /** getter to to check whether to use input or text area */
+  get isInput() {
+    return (
+      this.args.question.multiline === false ||
+      this.args.question.multiline === 'false'
+    );
+  }
 
-    @action
-    onChange(e){
-        this.args.onChange && this.args.onChange(event.target.value);
-    }
+  @action
+  onChange(e) {
+    this.args.onChange && this.args.onChange(event.target.value);
+  }
 }
--- app/helpers/contains.js
+++ app/helpers/contains.js
@@ -1,5 +1,7 @@
 import { helper } from '@ember/component/helper';
 
-export default helper(function contains([stack, needle, ...others ] /*, named*/) {
+export default helper(function contains(
+  [stack, needle, ...others] /*, named*/
+) {
   return stack && stack.includes(needle);
 });
