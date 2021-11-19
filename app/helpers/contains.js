9af897099540281036519f9cad616904be2faf67
Joshua Opata <opatajoshua@gmail.com>
2021-11-19 14:51:16 +0000

    added containes helper

---
+++ app/helpers/contains.js
@@ -0,0 +1,5 @@
+import { helper } from '@ember/component/helper';
+
+export default helper(function contains([stack, needle, ...others ] /*, named*/) {
+  return stack && stack.includes(needle);
+});
