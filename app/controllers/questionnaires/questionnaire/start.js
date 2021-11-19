1dd21f62690624bb3b65da2890939849d05d48db
Joshua Opata <opatajoshua@gmail.com>
2021-11-19 18:56:56 +0000

    implemented jump feature

--- app/controllers/questionnaires/questionnaire/start.js
+++ app/controllers/questionnaires/questionnaire/start.js
@@ -4,6 +4,12 @@ import { action } from '@ember/object';
 import Ember from 'ember';
 
 export default class QuestionnairesQuestionnaireStartController extends Controller {
+  /** a map of the loaded question's ids and their number we can jump to
+   * this will help to find what question number to easily jump to
+   * this will be loaded by route's setup controller
+  */
+  questionIdAndNumberMap={};
+
   /** keeping track of the current number */
   @tracked
   currentQuestionNumber = 1;
@@ -13,14 +19,18 @@ export default class QuestionnairesQuestionnaireStartController extends Controll
   answers = {};
 
   /** variable to hold error message */
-   @tracked
-   error = "";
+  @tracked
+  error = "";
 
   /** since its possible to jump we need to keep track of the answered questions in their order
    * so that when back button is click we can go to only the questions user previously answered
    */
   @tracked
   prevQuestionNumbers = [];
+
+  /** css class for the slider to slide left or right based on the current question number */
+  @tracked
+  sliderClass = Ember.String.htmlSafe('transform: translate3d(0%, 0, 0)');
 
   /** getter for the number of questions the current questionnaire has */
   get totalQuestions() {
@@ -32,9 +42,6 @@ export default class QuestionnairesQuestionnaireStartController extends Controll
     return (this.currentQuestionNumber / this.totalQuestions) * 100;
   }
 
-  /** css class for the slider to slide left or right based on the current question number */
-  @tracked
-  sliderClass = Ember.String.htmlSafe('transform: translate3d(0%, 0, 0)');
 
   /** main function that controls the slider*/
   slideToQuestion(qNumber) {
@@ -46,16 +53,27 @@ export default class QuestionnairesQuestionnaireStartController extends Controll
     this.sliderClass = Ember.String.htmlSafe(`transform: translate3d(-${100 * (qNumber - 1)}%, 0, 0)`);
   }
 
-  validateCurrentAnswer(){
-    const currQuest = this.model.questions[this.currentQuestionNumber-1];
-    const value = this.answers[currQuest.identifier];
+  /** getter for current question*/
+  get currentQuestion() {
+    return this.model.questions[this.currentQuestionNumber - 1];
+  }
+
+  /** gets answer for current question*/
+  get currentAnswer() {
+    return this.currentQuestion ? this.answers[this.currentQuestion.identifier] : null;
+  }
+
+  /** validates answer for the current question*/
+  validateCurrentAnswer() {
+    if (!this.currentQuestion)
+      return false;
 
     // check for required fields
-    if((currQuest.required===true || currQuest.required==='true') && (!value && value!=0)){
-      this.error="Diese Frage ist erforderlich."
+    if ((this.currentQuestion.required === true || this.currentQuestion.required === 'true') && (!this.currentAnswer && this.currentAnswer != 0)) {
+      this.error = "Diese Frage ist erforderlich."
       return false;
     }
-    this.error=""
+    this.error = ""
     return true;
   }
 
@@ -63,7 +81,7 @@ export default class QuestionnairesQuestionnaireStartController extends Controll
   @action
   jumpForwardToQuestion(qNumber) {
     if (this.currentQuestionNumber < qNumber) {
-      if(!this.validateCurrentAnswer())
+      if (!this.validateCurrentAnswer())
         return;
       this.prevQuestionNumbers.pushObject(this.currentQuestionNumber);
       this.slideToQuestion(this.currentQuestionNumber + 1);
@@ -74,10 +92,17 @@ export default class QuestionnairesQuestionnaireStartController extends Controll
   @action
   nextQuestion() {
     if (this.currentQuestionNumber < this.totalQuestions) {
-      if(!this.validateCurrentAnswer())
+      if (!this.validateCurrentAnswer())
         return;
+
+      let nextQuestNumb = this.currentQuestionNumber + 1
+      // check for jumps
+      if (this.currentQuestion.jumps && this.currentQuestion.jumps.length && this.currentAnswer) {
+        const jump = this.currentQuestion.jumps.find(e=>e.conditions.find(c=>c.value==this.currentAnswer)!=null)
+        nextQuestNumb = this.questionIdAndNumberMap[jump.destination.id]
+      }
       this.prevQuestionNumbers.pushObject(this.currentQuestionNumber);
-      this.slideToQuestion(this.currentQuestionNumber + 1);
+      this.slideToQuestion(nextQuestNumb);
     }
   }
 
@@ -94,7 +119,7 @@ export default class QuestionnairesQuestionnaireStartController extends Controll
     this.answers = { ...this.answers, [quest.identifier]: value };
 
     // go to next page if current question is a single select
-    if(quest.question_type==='multiple-choice' && (quest.multiple===false || quest.multiple==='false'))
+    if (quest.question_type === 'multiple-choice' && (quest.multiple === false || quest.multiple === 'false'))
       this.nextQuestion();
   }
 }
--- app/routes/questionnaires/questionnaire/start.js
+++ app/routes/questionnaires/questionnaire/start.js
@@ -5,10 +5,16 @@ export default class QuestionnairesQuestionnaireStartRoute extends Route {
     setupController(controller, model) {
         super.setupController(controller, model);
         // reset controller variables incase we load a different questionnaire record
-        this.controller.currentQuestionNumber = 1;
-        this.controller.answers = {};
-        this.controller.error = "";
-        this.controller.prevQuestionNumbers = [];
-        this.controller.sliderClass = Ember.String.htmlSafe('transform: translate3d(0%, 0, 0)');
+        controller.currentQuestionNumber = 1;
+        controller.answers = {};
+        controller.error = "";
+        controller.prevQuestionNumbers = [];
+        controller.sliderClass = Ember.String.htmlSafe('transform: translate3d(0%, 0, 0)');
+
+        // load questions id vs numbers map. this will help to find what question number to easily jump to
+        controller.questionIdAndNumberMap = model.questions.reduce((acc, curr, index)=>{
+            acc[curr.identifier]=index+1;
+            return acc;
+        }, {})
     }
 }
