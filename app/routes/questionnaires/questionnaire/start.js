affda4d907975b6be25cb66efcf3c4000caa43f9
Joshua Opata <opatajoshua@gmail.com>
2021-11-19 18:33:42 +0000

    reset start constroller variables

--- app/routes/questionnaires/questionnaire/start.js
+++ app/routes/questionnaires/questionnaire/start.js
@@ -1,3 +1,14 @@
 import Route from '@ember/routing/route';
+import Ember from 'ember';
 
-export default class QuestionnairesQuestionnaireStartRoute extends Route {}
+export default class QuestionnairesQuestionnaireStartRoute extends Route {
+    setupController(controller, model) {
+        super.setupController(controller, model);
+        // reset controller variables incase we load a different questionnaire record
+        this.controller.currentQuestionNumber = 1;
+        this.controller.answers = {};
+        this.controller.error = "";
+        this.controller.prevQuestionNumbers = [];
+        this.controller.sliderClass = Ember.String.htmlSafe('transform: translate3d(0%, 0, 0)');
+    }
+}
